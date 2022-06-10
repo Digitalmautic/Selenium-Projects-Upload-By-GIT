d33f52774ff99fcf3d4474acaf552a132b941e82
Rosan Kumar <digitalsmind07@gmail.com>
2022-06-10 10:39:35 +0530

    Done

--- A Java Grid Layout/src/gridlayoutcreation/AddDemoForGITHubToTest.java
+++ A Java Grid Layout/src/gridlayoutcreation/AddDemoForGITHubToTest.java
@@ -4,7 +4,12 @@ public class AddDemoForGITHubToTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    String Hello;
+    System.out.println();
+    
+    int a = 2;
+    int b = 3;
+    System.out.println(a+b);
 	}
 
 }
